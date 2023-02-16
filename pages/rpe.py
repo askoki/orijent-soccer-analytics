eb6706e983da71dbda881d2b8d49c18bbd852be1
askoki160 <arian.skoki160@gmail.com>
2023-02-16 18:34:49 +0100

    Refactor rpe page

--- pages/rpe.py
+++ pages/rpe.py
@@ -6,9 +6,10 @@ import matplotlib.patches as mpatches
 from oauth2client.service_account import ServiceAccountCredentials
 
 import pandas as pd
-from datetime import timedelta
 
 from helpers import authenticate
+from pages.rpe.constants import RPE1_COLOR, RPE23_COLOR, RPE46_COLOR, RPE78_COLOR, RPE9_COLOR, RPE10_COLOR
+from pages.rpe.helpers import get_rpe_questioneer_df, extract_players_rpe_mean_and_std, define_RPE_colors
 
 st.title("RPE")
 
@@ -29,41 +30,10 @@ if status:
         "auth_provider_x509_cert_url": st.secrets.google_api.auth_provider_x509_cert_url,
         "client_x509_cert_url": st.secrets.google_api.client_x509_cert_url,
     }, scope)
-    # import pdb;
-    #
-    # pdb.set_trace()
     client = gspread.authorize(creds)
 
     # Open the Google Sheets document
     sheet = client.open('NN RPE').sheet1
-
-
-    def get_rpe_questioneer_df(worksheet) -> pd.DataFrame:
-        rows = worksheet.get_all_values()
-        df = pd.DataFrame(rows)
-        df.columns = df.iloc[0]
-        df = df.iloc[1:]
-
-        df = df.rename(columns={
-            'Timestamp': 'timestamp',
-            'Имя Фамилия / Player': 'name',
-            'Дата / Session date': 'session_date',
-            'RPE': 'rpe'
-        })
-        df.name = df.name.str.replace('\t', '')
-        df.name = df.name.str.upper()
-        # reverse player names
-        # df.name = df.name.str.replace(r'(.+)\s(.+)', r'\2 \1')
-        df.loc[:, 'timestamp'] = pd.to_datetime(df.loc[:, 'timestamp'])
-        df = df.dropna()
-
-        df.loc[:, 'session_date'] = df.apply(lambda r: f'{r.session_date}/{r.timestamp.year}', axis=1)
-        df.loc[:, 'session_date'] = pd.to_datetime(df.session_date)
-
-        # move to the correct timezone
-        df.loc[:, 'timestamp'] = df.loc[:, 'timestamp'].apply(lambda r: r + timedelta(hours=1))
-        return df
-
 
     rpe_df = get_rpe_questioneer_df(worksheet=sheet)
     rpe_df = rpe_df.drop(columns=[
@@ -73,8 +43,8 @@ if status:
     rpe_df.timestamp = pd.to_datetime(rpe_df.timestamp, unit='ns', utc=False).dt.tz_localize(None)
 
     rpe_df.loc[:, 'name'] = rpe_df.name.apply(lambda r: cyrtranslit.to_latin(r, "ru") if r else r)
-    rpe_df.loc[:, 'session_date'] = pd.to_datetime(rpe_df.session_date).dt.date
-    rpe_df.loc[:, 'rpe'] = rpe_df.loc[:, 'rpe'].astype(int)
+    rpe_df.session_date = pd.to_datetime(rpe_df.session_date).dt.date
+    rpe_df.rpe = rpe_df.loc[:, 'rpe'].astype(int)
 
     session_dates = rpe_df.sort_values('session_date').session_date.unique()
     session_date = st.sidebar.selectbox('Select training date', session_dates, index=len(session_dates) - 1)
@@ -82,68 +52,6 @@ if status:
     session_df = rpe_df[rpe_df.session_date == session_date]
     session_df = session_df.drop_duplicates(subset=['name', 'session_date'], keep='last')
     session_df.reset_index(inplace=True, drop=True)
-
-
-    def extract_players_rpe_mean_and_std(session_df: pd.DataFrame, all_df: pd.DataFrame) -> tuple:
-        before_df = all_df[all_df.session_date < session_df.iloc[0].session_date]
-        players = session_df.name.unique()
-        mean_list = []
-        std_list = []
-        for player in players:
-            player_df = before_df[before_df.name == player]
-            mean_list.append(player_df.rpe.mean())
-            std_list.append(player_df.rpe.std())
-        return mean_list, std_list
-
-
-    def define_colors_depending_on_std(session_df: pd.DataFrame, mean_list: list, std_list: list) -> list:
-        players = session_df.name.unique()
-
-        DANGER = 'tomato'
-        WARNING = '#ffcc00'
-        NORMAL = 'skyblue'
-        colors = []
-        for player, p_mean, p_std in zip(players, mean_list, std_list):
-            value = session_df[session_df.name == player].rpe.iloc[0]
-
-            if ((p_mean - 2 * p_std) > value) or (value > (p_mean + 2 * p_std)):
-                colors.append(DANGER)
-            elif ((p_mean - p_std) > value) or (value > (p_mean + p_std)):
-                colors.append(WARNING)
-            else:
-                colors.append(NORMAL)
-        return colors
-
-
-    RPE1_COLOR = '#5895fe'
-    RPE23_COLOR = '#78e0df'
-    RPE46_COLOR = '#87e740'
-    RPE78_COLOR = '#f5c545'
-    RPE9_COLOR = '#e98031'
-    RPE10_COLOR = '#e35022'
-
-    RPE_COLOR_DICT = {
-        1: RPE1_COLOR,
-        2: RPE23_COLOR,
-        3: RPE23_COLOR,
-        4: RPE46_COLOR,
-        5: RPE46_COLOR,
-        6: RPE46_COLOR,
-        7: RPE78_COLOR,
-        8: RPE78_COLOR,
-        9: RPE9_COLOR,
-        10: RPE10_COLOR,
-    }
-
-
-    def define_RPE_colors(values: list) -> list:
-        colors = []
-        for value in values:
-            for rpe_value, rpe_color in RPE_COLOR_DICT.items():
-                if int(round(value, 0)) == rpe_value:
-                    colors.append(rpe_color)
-        return colors
-
 
     fig, ax = plt.subplots(figsize=(14, 8), nrows=1, ncols=1)
     players = session_df.name.unique()
---
+++ pages/rpe/helpers.py
@@ -0,0 +1,70 @@
+from datetime import timedelta
+from pages.rpe.constants import RPE_COLOR_DICT
+import pandas as pd
+
+
+def get_rpe_questioneer_df(worksheet) -> pd.DataFrame:
+    rows = worksheet.get_all_values()
+    df = pd.DataFrame(rows)
+    df.columns = df.iloc[0]
+    df = df.iloc[1:]
+
+    df = df.rename(columns={
+        'Timestamp': 'timestamp',
+        'Имя Фамилия / Player': 'name',
+        'Дата / Session date': 'session_date',
+        'RPE': 'rpe'
+    })
+    df.name = df.name.str.replace('\t', '')
+    df.name = df.name.str.upper()
+    # reverse player names
+    # df.name = df.name.str.replace(r'(.+)\s(.+)', r'\2 \1')
+    df.timestamp = pd.to_datetime(df.loc[:, 'timestamp'])
+    df = df.dropna()
+
+    df.loc[:, 'session_date'] = df.apply(lambda r: f'{r.session_date}/{r.timestamp.year}', axis=1)
+    df.session_date = pd.to_datetime(df.session_date)
+
+    # move to the correct timezone
+    df.timestamp = df.loc[:, 'timestamp'].apply(lambda r: r + timedelta(hours=1))
+    return df
+
+
+def extract_players_rpe_mean_and_std(session_df: pd.DataFrame, all_df: pd.DataFrame) -> tuple:
+    before_df = all_df[all_df.session_date < session_df.iloc[0].session_date]
+    players = session_df.name.unique()
+    mean_list = []
+    std_list = []
+    for player in players:
+        player_df = before_df[before_df.name == player]
+        mean_list.append(player_df.rpe.mean())
+        std_list.append(player_df.rpe.std())
+    return mean_list, std_list
+
+
+def define_colors_depending_on_std(session_df: pd.DataFrame, mean_list: list, std_list: list) -> list:
+    players = session_df.name.unique()
+
+    DANGER = 'tomato'
+    WARNING = '#ffcc00'
+    NORMAL = 'skyblue'
+    colors = []
+    for player, p_mean, p_std in zip(players, mean_list, std_list):
+        value = session_df[session_df.name == player].rpe.iloc[0]
+
+        if ((p_mean - 2 * p_std) > value) or (value > (p_mean + 2 * p_std)):
+            colors.append(DANGER)
+        elif ((p_mean - p_std) > value) or (value > (p_mean + p_std)):
+            colors.append(WARNING)
+        else:
+            colors.append(NORMAL)
+    return colors
+
+
+def define_RPE_colors(values: list) -> list:
+    colors = []
+    for value in values:
+        for rpe_value, rpe_color in RPE_COLOR_DICT.items():
+            if int(round(value, 0)) == rpe_value:
+                colors.append(rpe_color)
+    return colors
